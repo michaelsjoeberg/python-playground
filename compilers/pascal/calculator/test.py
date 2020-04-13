5be03940567478143746b1eaf769d3eff05af650
Michael Sjöberg <random@michaelsjoberg.com>
2020-04-13 23:41:20 +0200

    Update test.py

--- compilers/pascal/calculator/test.py
+++ compilers/pascal/calculator/test.py
@@ -1,4 +1,5 @@
 from calculator import Lexer
+from calculator import Parser
 from calculator import Interpreter
 
 #lexer = Lexer('5 + 5')
@@ -10,10 +11,6 @@ from calculator import Interpreter
 # ---------------------------------------------------
 # Asserts
 # ---------------------------------------------------
-assert (Interpreter(Lexer('3')).expr() == 3)
-assert (Interpreter(Lexer('2 + 7 * 4')).expr() == 30)
-assert (Interpreter(Lexer('7 - 8 / 4')).expr() == 5)
-assert (Interpreter(Lexer('14 + 2 * 3 - 6 / 2')).expr() == 17)
-assert (Interpreter(Lexer('7 + 3 * (10 / (12 / (3 + 1) - 1))')).expr() == 22)
-assert (Interpreter(Lexer('7 + 3 * (10 / (12 / (3 + 1) - 1)) / (2 + 3) - 5 - 3 + (8)')).expr() == 10)
-assert (Interpreter(Lexer('7 + (((3 + 2)))')).expr() == 12)
+assert (Interpreter(Parser(Lexer('7 + 3 * (10 / (12 / (3 + 1) - 1))'))).interpret() == 22)
+assert (Interpreter(Parser(Lexer('7 + 3 * (10 / (12 / (3 + 1) - 1)) / (2 + 3) - 5 - 3 + (8)'))).interpret() == 10)
+assert (Interpreter(Parser(Lexer('7 + (((3 + 2)))'))).interpret() == 12)
