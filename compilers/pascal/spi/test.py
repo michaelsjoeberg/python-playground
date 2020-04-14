6bd3bd7b2e26b5154fce29859df1ced218c81007
Michael Sjöberg <random@michaelsjoberg.com>
2020-04-14 02:07:58 +0200

    Update test.py

--- compilers/pascal/spi/test.py
+++ compilers/pascal/spi/test.py
@@ -1,6 +1,6 @@
-from calculator import Lexer
-from calculator import Parser
-from calculator import Interpreter
+from spi import Lexer
+from spi import Parser
+from spi import Interpreter
 
 #lexer = Lexer('5 + 5')
 # print(lexer.get_next_token())
@@ -11,6 +11,14 @@ from calculator import Interpreter
 # ---------------------------------------------------
 # Asserts
 # ---------------------------------------------------
+#Interpreter(Parser(Lexer(text))).interpret()
+
 assert (Interpreter(Parser(Lexer('7 + 3 * (10 / (12 / (3 + 1) - 1))'))).interpret() == 22)
 assert (Interpreter(Parser(Lexer('7 + 3 * (10 / (12 / (3 + 1) - 1)) / (2 + 3) - 5 - 3 + (8)'))).interpret() == 10)
 assert (Interpreter(Parser(Lexer('7 + (((3 + 2)))'))).interpret() == 12)
+
+# negation
+assert (Interpreter(Parser(Lexer('- 3'))).interpret() == -3)
+assert (Interpreter(Parser(Lexer('+ 3'))).interpret() == 3)
+assert (Interpreter(Parser(Lexer('5 - - - + - 3'))).interpret() == 8)
+assert (Interpreter(Parser(Lexer('5 - - - + - (3 + 4) - + 2'))).interpret() == 10)
