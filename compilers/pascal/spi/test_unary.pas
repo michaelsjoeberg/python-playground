3452124268d6b75ae17c62573ac65c2ace8b3490
Michael Sjöberg <random@michaelsjoberg.com>
2020-04-14 02:08:00 +0200

    Create test_unary.pas

---
+++ compilers/pascal/spi/test_unary.pas
@@ -0,0 +1,10 @@
+{ http://www.freepascal.org/ }
+{ compile and run: $ fpc testunary.pas && ./testunary }
+program testunary;
+
+begin
+   writeln('Expected -3. Got ', - 3);
+   writeln('Expected 3. Got ', + 3);
+   writeln('Expected 8. Got ', 5 - - - + - 3);
+   writeln('Expected 10. Got ', 5 - - - + - (3 + 4) - +2);
+end.
